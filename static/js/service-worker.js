e8ad9e6dd97dae39fa25cb935f4a5d1e0dc66e1c
stephane-duteriez <stephane.duteriez@gmail.com>
2019-11-03 09:05:23 +0100

    stable before redis

--- static/js/service-worker.js
+++ static/js/service-worker.js
@@ -10,10 +10,19 @@ self.addEventListener('install', function(event) {
 });
 
 
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-  });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request).then((response) => {
+        let responseClone = response.clone();
+        caches.open('v1').then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      });
+    }).catch(() => {
+      return caches.match('./sw-test/gallery/myLittleVader.jpg');
+    })
+  );
+});
